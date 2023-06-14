535baab61f03407086f160d69cbcd2a72a0c696e
Italo Costa <italocostajobs@gmail.com>
2023-06-14 19:41:06 -0300

    Praticing Methods in Java

---
+++ src/java_basics/LearningMethods.java
@@ -0,0 +1,14 @@
+package java_basics;
+
+public class LearningMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dhsajdhasjda");
+		System.out.println(MyUtils.printSomeJunk("Some text"));
+		MyUtils.sum2Numbers(4, 21);
+		int myVar = MyUtils.add10(4321);
+		System.out.print(myVar);
+	}
+
+}
---
+++ src/java_basics/MyUtils.java
@@ -0,0 +1,21 @@
+package java_basics;
+
+public class MyUtils {
+
+	public static String printSomeJunk(String argument) {
+		return argument;
+	}
+
+	public static int printSomeJunk(int argument) {
+		return argument;
+	}
+
+	public static void sum2Numbers(int firstArg, int secondArg) {
+		System.out.println(firstArg + secondArg);
+	}
+
+	public static int add10(int someArgument) {
+		int result = someArgument + 10;
+		return result;
+	}
+}
